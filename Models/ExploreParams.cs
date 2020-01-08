d7ce981729d63089bb7eef52c08fd428b9592223
AndreiBozantan <andrei.bozantan@gmail.com>
2020-01-08 20:42:43 +0200

    modified the JSON keys for the explore endpoint so that they will match with the corresponding C# identifiers

--- Models/ExploreParams.cs
+++ Models/ExploreParams.cs
@@ -6,18 +6,12 @@ namespace Explorer.Models
     public class ExploreParams
     {
         [Required]
-        [Display(Name = "api_key")]
-        [JsonPropertyName("api_key")]
         public string ApiKey { get; set; }
 
         [Required]
-        [Display(Name = "table_name")]
-        [JsonPropertyName("table_name")]
         public string TableName {get; set; }
 
         [Required]
-        [Display(Name = "column_name")]
-        [JsonPropertyName("column_name")]
         public string ColumnName {get; set; }
     }
 }
