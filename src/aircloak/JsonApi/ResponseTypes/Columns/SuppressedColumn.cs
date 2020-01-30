b45a83bdc2878a76976276443844f838b774a6d1
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-01-30 16:38:14 +0000

    Add doc comments.

--- src/aircloak/JsonApi/ResponseTypes/Columns/SuppressedColumn.cs
+++ src/aircloak/JsonApi/ResponseTypes/Columns/SuppressedColumn.cs
@@ -1,9 +1,21 @@
 namespace Aircloak.JsonApi.ResponseTypes
 {
+    /// <summary>
+    /// Represents a column value that has been suppressed by Diffix anonymization.
+    /// </summary>
+    /// <typeparam name="T">The expected type of the column value.</typeparam>
     public class SuppressedColumn<T> : AircloakColumn<T>
     {
+        /// <summary>
+        /// Gets a value indicating whether the column value was suppressed.
+        /// Always returns true since this particular column has been suppressed.
+        /// </summary>
         public override bool IsSuppressed => true;
 
+        /// <summary>
+        /// Gets a value indicating whether the column value was NULL.
+        /// Always returns false since this particular column has been suppressed.
+        /// </summary>
         public override bool IsNull => false;
     }
 }
--- src/aircloak/JsonApi/ResponseTypes/Columns/ValueColumn.cs
+++ src/aircloak/JsonApi/ResponseTypes/Columns/ValueColumn.cs
@@ -1,18 +1,35 @@
 namespace Aircloak.JsonApi.ResponseTypes
 {
-    using System.Text.Json;
-
+    /// <summary>
+    /// Represents an unsuppressed column value.
+    /// </summary>
+    /// <typeparam name="T">The type of the column value.</typeparam>
     public class ValueColumn<T> : AircloakColumn<T>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValueColumn{T}"/> class.
+        /// </summary>
+        /// <param name="columnValue">The column's value.</param>
         public ValueColumn(T columnValue)
         {
             ColumnValue = columnValue;
         }
 
+        /// <summary>
+        /// Gets a value indicating whether the column value was suppressed.
+        /// Always returns false since the column is not suppressed.
+        /// </summary>
         public override bool IsSuppressed => false;
 
+        /// <summary>
+        /// Gets a value indicating whether the column value was NULL.
+        /// Always returns false since the column has a value.
+        /// </summary>
         public override bool IsNull => false;
 
+        /// <summary>
+        /// Gets the column's value.
+        /// </summary>
         public T ColumnValue { get; private set; }
     }
 }
