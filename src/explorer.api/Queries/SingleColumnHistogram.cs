f90707926093dc41bd15a5189e3ffadee2fca20a
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-01-30 16:59:35 +0000

    Single column histogram query def and tests.

---
+++ src/explorer.api/Queries/SingleColumnHistogram.cs
@@ -0,0 +1,88 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("explorer.api.tests")]
+
+namespace Explorer.Queries
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.Json;
+
+    using Aircloak.JsonApi;
+    using Aircloak.JsonApi.ResponseTypes;
+
+    internal class SingleColumnHistogram :
+        IQuerySpec<SingleColumnHistogram.Result>
+    {
+        public SingleColumnHistogram(
+            string tableName,
+            string columnName,
+            IList<decimal> buckets)
+        {
+            TableName = tableName;
+            ColumnName = columnName;
+            Buckets = buckets;
+        }
+
+        public string QueryStatement
+        {
+            get
+            {
+                var bucketsFragment = string.Join(
+                    ",",
+                    from bucket in Buckets select $"bucket({ColumnName} by {bucket})");
+
+                return $@"
+                        select
+                            grouping_id(
+                                {bucketsFragment}
+                            ),
+                            {Buckets.Count} as num_buckets,
+                            {bucketsFragment},
+                            count(*),
+                            count_noise(*)
+                        from {TableName}
+                        group by grouping sets ({Enumerable.Range(3, Buckets.Count)})";
+            }
+        }
+
+        public string TableName { get; }
+
+        public string ColumnName { get; }
+
+        public IList<decimal> Buckets { get; }
+
+        public class Result : IJsonArrayConvertible
+        {
+            public decimal? BucketSize { get; set; }
+
+            public AircloakColumn<decimal> LowerBound { get; set; }
+
+            public int? Count { get; set; }
+
+            public double? CountNoise { get; set; }
+
+            void IJsonArrayConvertible.FromArrayValues(ref Utf8JsonReader reader)
+            {
+                reader.Read();
+                var groupingFlags = reader.GetInt32();
+                reader.Read();
+                var numBuckets = reader.GetInt32();
+
+                for (var i = 0; i < numBuckets; i++)
+                {
+                    reader.Read();
+                    if (((groupingFlags >> i) & 1) == 0)
+                    {
+                        LowerBound = AircloakColumnJsonParser.ParseDecimal(ref reader);
+                    }
+                }
+
+                reader.Read();
+                Count = reader.GetInt32();
+                reader.Read();
+                CountNoise = reader.GetInt32();
+            }
+        }
+    }
+}
--- tests/explorer.api.tests/QueryTests.cs
+++ tests/explorer.api.tests/QueryTests.cs
@@ -1,6 +1,7 @@
 namespace Explorer.Queries.Tests
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Aircloak.JsonApi;
@@ -46,10 +47,7 @@ namespace Explorer.Queries.Tests
                     tableName: "loans",
                     columnName: "payments"));
 
-            Assert.All(realResult.ResultRows, row =>
-            {
-                Assert.True(row.Count.HasValue && row.Count > 0);
-            });
+            Assert.All(realResult.ResultRows, row => Assert.True(row.Count.HasValue && row.Count > 0));
 
             var textResult = await QueryResult<DistinctColumnValues.TextResult>(
                 new DistinctColumnValues(
@@ -66,7 +64,28 @@ namespace Explorer.Queries.Tests
             Assert.True(textResult.ResultRows.Count() == 2);
         }
 
-        private async Task<QueryResult<TResult>> QueryResult<TResult>(DistinctColumnValues query)
+        [Fact]
+        public async void TestHistogramQuery()
+        {
+            var bucketSizes = new List<decimal> { 10_000, 20_000, 50_000 };
+            var result = await QueryResult<SingleColumnHistogram.Result>(
+                new SingleColumnHistogram(
+                    "loans",
+                    "amount",
+                    bucketSizes));
+
+            Assert.All(result.ResultRows, row =>
+            {
+                Assert.True(bucketSizes.Exists(v => row.BucketSize?.Equals(v) ?? false));
+                Assert.True(row.LowerBound.IsNull ||
+                            row.LowerBound.IsSuppressed ||
+                            ((ValueColumn<decimal>)row.LowerBound).ColumnValue >= 0);
+                Assert.True(row.Count.HasValue && row.Count > 0);
+                Assert.True(row.CountNoise.HasValue);
+            });
+        }
+
+        private async Task<QueryResult<TResult>> QueryResult<TResult>(IQuerySpec<TResult> query)
             where TResult : IJsonArrayConvertible, new()
         {
             return await jsonApiSession.Query<TResult>(
