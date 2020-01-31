35566e9303657fcad8c3d50499200cca0af3b679
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-01-31 18:25:01 +0000

    Add ResolveQuery member to ColumnExplorer
    Permits more succinct queries.

--- src/explorer.api/Explorers/ColumnExplorer.cs
+++ src/explorer.api/Explorers/ColumnExplorer.cs
@@ -2,9 +2,12 @@ namespace Explorer
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     using Aircloak.JsonApi;
+    using Aircloak.JsonApi.ResponseTypes;
     using Explorer.Api.Models;
+    using Explorer.Queries;
 
     internal abstract class ColumnExplorer
     {
@@ -22,5 +25,16 @@ namespace Explorer
         protected JsonApiClient ApiClient { get; }
 
         public abstract IAsyncEnumerable<ExploreResult> Explore();
+
+        protected async Task<QueryResult<TResult>> ResolveQuery<TResult>(
+            IQuerySpec<TResult> query,
+            TimeSpan timeout)
+                where TResult : IJsonArrayConvertible, new()
+        {
+            return await ApiSession.Query<TResult>(
+                ExploreParams.DataSourceName,
+                query.QueryStatement,
+                timeout);
+        }
     }
 }
