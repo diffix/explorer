90faf108d8c3c69700cc6607c4fc9dd9b408427e
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-02-05 13:04:54 +0000

    Fix analyzer warnings.

--- src/explorer.api/Queries/BucketSize.cs
+++ src/explorer.api/Queries/BucketSize.cs
@@ -1,10 +1,13 @@
+#pragma warning disable SA1137 // Elements should have the same indentation
+
 namespace Explorer.Queries
 {
     using System.Linq;
 
     public class BucketSize
     {
-        private static readonly decimal[] ValidSizes = {
+        private static readonly decimal[] ValidSizes =
+        {
                      0.0001M,                0.0002M,                0.0005M,
                       0.001M,                 0.002M,                 0.005M,
                        0.01M,                  0.02M,                  0.05M,
@@ -21,22 +24,55 @@ namespace Explorer.Queries
             1_000_000_000.0M,       2_000_000_000.0M,       5_000_000_000.0M,
            10_000_000_000.0M,      20_000_000_000.0M,      50_000_000_000.0M,
           100_000_000_000.0M,     200_000_000_000.0M,     500_000_000_000.0M,
-        1_000_000_000_000.0M,   2_000_000_000_000.0M,   5_000_000_000_000.0M
+        1_000_000_000_000.0M,   2_000_000_000_000.0M,   5_000_000_000_000.0M,
         };
-
-        public decimal SnappedSize { get; }
 
         public BucketSize(long size)
             : this((decimal)size)
-        { }
+        {
+        }
 
         public BucketSize(double size)
             : this((decimal)size)
-        { }
+        {
+        }
 
         public BucketSize(decimal size)
         {
             SnappedSize = Snap(size);
+        }
+
+        public decimal SnappedSize { get; }
+
+        public static decimal Snap(decimal size)
+        {
+            if (size <= 0)
+            {
+                throw new System.ArgumentException($"Invalid bucket size {size}");
+            }
+
+            if (size < ValidSizes[0])
+            {
+                return ValidSizes[0];
+            }
+
+            if (size > ValidSizes.Last())
+            {
+                return ValidSizes.Last();
+            }
+
+            if (ValidSizes.Contains(size))
+            {
+                return size;
+            }
+
+            var (below, above) = ValidSizes.Zip(ValidSizes.Skip(1)).First(tup =>
+                size >= tup.First && size <= tup.Second);
+
+            var lower_diff = size - below;
+            var upper_diff = above - size;
+
+            return lower_diff < upper_diff ? below : above;
         }
 
         public BucketSize Larger(int steps)
@@ -56,32 +92,6 @@ namespace Explorer.Queries
                     .ElementAt(steps));
         }
 
-        public static decimal Snap(decimal size)
-        {
-            if (size <= 0)
-                throw new System.ArgumentException($"Invalid bucket size {size}");
-
-            if (size < ValidSizes.First())
-                return ValidSizes.First();
-
-            if (size > ValidSizes.Last())
-                return ValidSizes.Last();
-
-            if (ValidSizes.Contains(size))
-            {
-                return size;
-            }
-
-            var (below, above) = ValidSizes.Zip(ValidSizes.Skip(1)).First(tup =>
-                size >= tup.First && size <= tup.Second
-            );
-
-            var lower_diff = size - below;
-            var upper_diff = above - size;
-
-            return lower_diff < upper_diff ? below : above;
-        }
-
         public override bool Equals(object? obj)
         {
             if (obj == null || GetType() != obj.GetType())
@@ -95,3 +105,5 @@ namespace Explorer.Queries
         public override int GetHashCode() => SnappedSize.GetHashCode();
     }
 }
+
+#pragma warning restore SA1137 // Elements should have the same indentation
