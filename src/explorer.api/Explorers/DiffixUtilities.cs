2915cfe6a232e686eedd15351939b601358c4b91
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-02-19 10:58:38 +0000

    Refactor DiffixUtilities.EstimateBucketResolutions
    Avoid unnecessary use of dynamic.

--- src/explorer.api/Explorers/DiffixUtilities.cs
+++ src/explorer.api/Explorers/DiffixUtilities.cs
@@ -10,33 +10,52 @@ namespace Explorer
     {
         internal static List<decimal> EstimateBucketResolutions(
             long numSamples,
-            dynamic minSample,
-            dynamic maxSample,
+            double minSample,
+            double maxSample,
             long valuesPerBucketTarget)
         {
             Debug.Assert(numSamples > 0, "Argument numSamples should always be greater than zero.");
 
-            double valueDensity;
-            try
-            {
-                valueDensity = (double)numSamples / (maxSample - minSample);
-            }
-            catch (System.DivideByZeroException)
-            {
-                return new List<decimal> { 1M };
-            }
+            var range = maxSample - minSample;
 
-            var bucketSizeEstimate = new BucketSize(valuesPerBucketTarget / valueDensity);
+            Debug.Assert(range > 0, "Data range must be greater than zero.");
 
-            return (
-                from bucketSize in new List<BucketSize>
-                {
-                    bucketSizeEstimate.Smaller(steps: 2),
-                    bucketSizeEstimate,
-                    bucketSizeEstimate.Larger(steps: 2),
-                }
-                select bucketSize.SnappedSize)
-                .ToList();
+            var valueDensity = (double)numSamples / (maxSample - minSample);
+
+            return EstimateBucketResolutions(valuesPerBucketTarget, valueDensity);
+        }
+
+        internal static List<decimal> EstimateBucketResolutions(
+            long numSamples,
+            long minSample,
+            long maxSample,
+            long valuesPerBucketTarget)
+        {
+            Debug.Assert(numSamples > 0, "Argument numSamples should always be greater than zero.");
+
+            var range = maxSample - minSample;
+
+            Debug.Assert(range > 0, "Data range must be greater than zero.");
+
+            var valueDensity = (double)numSamples / (maxSample - minSample);
+
+            return EstimateBucketResolutions(valuesPerBucketTarget, valueDensity);
+        }
+
+        private static List<decimal> EstimateBucketResolutions(
+            long valuesPerBucketTarget,
+            double valueDensityEstimate)
+        {
+            var bucketSizeEstimate = new BucketSize(valuesPerBucketTarget / valueDensityEstimate);
+
+            var bucketList = new List<BucketSize>
+            {
+                bucketSizeEstimate.Smaller(steps: 2),
+                bucketSizeEstimate,
+                bucketSizeEstimate.Larger(steps: 2),
+            };
+
+            return bucketList.Select(x => x.SnappedSize).ToList();
         }
     }
 }
