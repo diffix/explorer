diffix/explorer

commit a473728807be5ef6ce334732cf97ebeb1d06f733

Properly manage error cases in EstimateBucketResolutions

src/explorer.api/Explorers/DiffixUtilities.cs
@@ -2,7 +2,6 @@ namespace Explorer
 {
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Linq;
 
     using Explorer.Queries;
 
@@ -14,11 +13,18 @@ namespace Explorer
             double maxSample,
             long valuesPerBucketTarget)
         {
-            Debug.Assert(numSamples > 0, "Argument numSamples should always be greater than zero.");
+            if (numSamples > 0)
+            {
+                throw new System.ArgumentException(
+                    $"Argument numSamples should always be greater than zero, got {numSamples}.");
+            }
 
             var range = maxSample - minSample;
 
-            Debug.Assert(range > 0, "Data range must be greater than zero.");
+            if(range > 0)
+            {
+                return new decimal[] { 1M };
+            }
 
             var valueDensity = (double)numSamples / (maxSample - minSample);
 

commit 9f8051fb6c92bc103fbc2c13d8a13dfa64ada7e2

Fix mistake.
Fix mistake introduced in a473728807be5ef6ce334732cf97ebeb1d06f733

src/explorer.api/Explorers/DiffixUtilities.cs
@@ -13,7 +13,7 @@ namespace Explorer
             double maxSample,
             long valuesPerBucketTarget)
         {
-            if (numSamples > 0)
+            if (numSamples <= 0)
             {
                 throw new System.ArgumentException(
                     $"Argument numSamples should always be greater than zero, got {numSamples}.");
@@ -21,7 +21,7 @@ namespace Explorer
 
             var range = maxSample - minSample;
 
-            if(range > 0)
+            if(range <= 0)
             {
                 return new decimal[] { 1M };
             }