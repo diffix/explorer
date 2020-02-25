316afcf5aef50c44f207e2fb6c750b5a04146724
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-02-25 16:29:31 +0000

    Add RecordingOptions enum.

---
+++ src/vcr-sharp/RecordingOptions.cs
@@ -0,0 +1,12 @@
+namespace VcrSharp
+{
+    public enum RecordingOptions
+    {
+        /// <summary>Record all interactions.</summary>
+        RecordAll,
+        /// <summary>Record successful interactions only.</summary>
+        SuccessOnly,
+        /// <summary>Record unsuccessful interactions only.</summary>
+        FailureOnly,
+    }
+}
