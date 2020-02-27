6f3087f19f7a50a133877626e5bbdf0a941294ac
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-02-27 20:30:25 +0000

    Add dependency injection helper for Authentication

---
+++ src/aircloak/JsonApi/Extensions/ServiceRegistration.cs
@@ -0,0 +1,29 @@
+namespace Aircloak.JsonApi
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Extension methods for service registration unsing Microsoft's dependecy injection framework.
+    /// </summary>
+    public static class ServiceRegistration
+    {
+        /// <summary>
+        /// Injects and configures the <see cref="JsonApiClient"/>.
+        /// </summary>
+        /// <param name="services">The IServiceCollection.</param>
+        /// <param name="apiBaseAddress">The base Url for the aircloak api.</param>
+        /// <typeparam name="TAuthHandler">
+        /// A type that implements <see cref="AircloakAuthenticationHandler"/>.
+        /// </typeparam>
+        public static IHttpClientBuilder AddAircloakJsonApiServices<TAuthHandler>(
+            this IServiceCollection services,
+            System.Uri apiBaseAddress)
+        where TAuthHandler : AircloakAuthenticationHandler
+        {
+            return services
+                .AddScoped<TAuthHandler>()
+                .AddHttpClient<JsonApiClient>(client => client.BaseAddress = apiBaseAddress)
+                .AddHttpMessageHandler<TAuthHandler>();
+        }
+    }
+}
