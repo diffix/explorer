ccbb6edc340d8fdd09dabfe430f0cc0261c86935
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-03-04 11:32:38 +0000

    Add basic skeleton for datetime explorer.

---
+++ src/explorer.api/Explorers/DatetimeColumnExplorer.cs
@@ -0,0 +1,83 @@
+﻿namespace Explorer
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Explorer.Queries;
+
+    internal class DatetimeColumnExplorer : ExplorerBase
+    {
+        // TODO: The following should be configuration items (?)
+        private const long ValuesPerBucketTarget = 20;
+
+        private const double SuppressedRatioThreshold = 0.1;
+
+        public DatetimeColumnExplorer(IQueryResolver queryResolver, string tableName, string columnName)
+            : base(queryResolver)
+        {
+            TableName = tableName;
+            ColumnName = columnName;
+        }
+
+        private string TableName { get; }
+
+        private string ColumnName { get; }
+
+        public override async Task Explore()
+        {
+            var stats = (await ResolveQuery<NumericColumnStats.Result<DateTime>>(
+                new NumericColumnStats(TableName, ColumnName),
+                timeout: TimeSpan.FromMinutes(2)))
+                .ResultRows
+                .Single();
+
+            PublishMetric(new UntypedMetric(name: "naive_min", metric: stats.Min));
+            PublishMetric(new UntypedMetric(name: "naive_max", metric: stats.Max));
+
+            var distinctValueQ = await ResolveQuery<DistinctColumnValues.Result<DateTime>>(
+                new DistinctColumnValues(TableName, ColumnName),
+                timeout: TimeSpan.FromMinutes(2));
+
+            var suppressedValueCount = distinctValueQ.ResultRows.Sum(row =>
+                    row.DistinctData.IsSuppressed ? row.Count : 0);
+
+            var totalValueCount = stats.Count;
+
+            if (totalValueCount == 0)
+            {
+                throw new Exception(
+                    $"Total value count for {TableName}, {ColumnName} is zero.");
+            }
+
+            var suppressedValueRatio = (double)suppressedValueCount / totalValueCount;
+
+            if (suppressedValueRatio < SuppressedRatioThreshold)
+            {
+                // Only few of the values are suppressed. This means the data is already well-segmented and can be
+                // considered categorical or quasi-categorical.
+                var distinctValues =
+                    from row in distinctValueQ.ResultRows
+                    where !row.DistinctData.IsSuppressed
+                    select new
+                    {
+                        row.DistinctData.Value,
+                        row.Count,
+                    };
+
+                PublishMetric(new UntypedMetric(name: "distinct_values", metric: distinctValues));
+                PublishMetric(new UntypedMetric(name: "suppressed_values", metric: suppressedValueCount));
+            }
+
+            // var bucketsToSample = DiffixUtilities.EstimateBucketResolutions(
+            //     stats.Count, stats.Min, stats.Max, ValuesPerBucketTarget);
+
+            // Dates histogram (date_trunc by year, quarter, month, day, hour, minute, and second
+            //   ==> might as well query all at same time and then keep the most useful)
+            // Cyclical Histogram (ie. year(), quarter(), month(), day(), hour(), minute(), second(), weekday()
+            //   ==> same as above: just query everything and decide what to do with it after.)
+            // Median
+            // Average
+        }
+    }
+}
