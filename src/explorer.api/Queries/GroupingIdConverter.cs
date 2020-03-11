e4bb1d94905dfafe9c78a7fd8fa3feba5cf4d406
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-03-11 14:50:53 +0000

    Add utilities for dealing with airlcoak grouping_id()

---
+++ src/explorer.api/Queries/GroupingIdConverter.cs
@@ -0,0 +1,70 @@
+﻿namespace Explorer.Queries
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GroupingIdConverter
+    {
+        private readonly int groupMask;
+
+        private readonly int groupSize;
+
+        public GroupingIdConverter(int groupSize)
+        {
+            if (groupSize < 1 || groupSize > sizeof(int) * 8)
+            {
+                throw new System.ArgumentOutOfRangeException(
+                    $"Group size must be in range [{1} {sizeof(int) * 8}].");
+            }
+
+            var mask = 0;
+            for (var i = 0; i < groupSize; i++)
+            {
+                mask |= 1 << i;
+            }
+
+            groupMask = mask;
+            this.groupSize = groupSize;
+        }
+
+        public int GroupingIdFromIndex(int index)
+        {
+            if (index >= groupSize || index < 0)
+            {
+                throw new System.ArgumentOutOfRangeException(
+                    $"Index {index} is not within valid range [0 {groupSize}].");
+            }
+
+            return (1 << (groupSize - index - 1)) ^ groupMask;
+        }
+
+        public int SingleIndexFromGroupingId(int groupingId)
+        {
+            return IndicesFromGroupingId(groupingId).Single();
+        }
+
+        public IEnumerable<int> IndicesFromGroupingId(int groupingId)
+        {
+            if (groupingId < 0 || groupingId > groupMask)
+            {
+                throw new System.ArgumentOutOfRangeException(
+                    $"Grouping id must be in range [{1} {groupMask}]. Got {groupingId}.");
+            }
+
+            return IndexIterator();
+
+            IEnumerable<int> IndexIterator()
+            {
+                var onesMask = groupingId ^ groupMask;
+                for (var i = 0; i < groupSize; i++)
+                {
+                    var offset = groupSize - 1 - i;
+                    if ((1 << offset & onesMask) > 0)
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+    }
+}
