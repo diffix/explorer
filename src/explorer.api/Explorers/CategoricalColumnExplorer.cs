4e5f9b9009866100ad67266702b3daa9e2b7e968
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-03-18 14:41:23 +0000

    Add new generic explorer for categorical columns. Use this for text and bool column types.

---
+++ src/explorer.api/Explorers/CategoricalColumnExplorer.cs
@@ -0,0 +1,58 @@
+namespace Explorer
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Explorer.Diffix.Extensions;
+    using Explorer.Queries;
+
+    internal class CategoricalColumnExplorer : ExplorerBase
+    {
+        public CategoricalColumnExplorer(IQueryResolver queryResolver, string tableName, string columnName)
+            : base(queryResolver)
+        {
+            TableName = tableName;
+            ColumnName = columnName;
+        }
+
+        private string TableName { get; }
+
+        private string ColumnName { get; }
+
+        public override async Task Explore(CancellationToken cancellationToken)
+        {
+            var distinctValuesQ = await ResolveQuery<DistinctColumnValues.Result>(
+                new DistinctColumnValues(TableName, ColumnName),
+                cancellationToken);
+
+            var (totalValueCount, suppressedValueCount) = distinctValuesQ.ResultRows.CountTotalAndSuppressed();
+
+            PublishMetric(new UntypedMetric(name: "suppressed_values", metric: suppressedValueCount));
+
+            // This shouldn't happen, but check anyway.
+            if (totalValueCount == 0)
+            {
+                throw new Exception(
+                    $"Total value count for {TableName}, {ColumnName} is zero.");
+            }
+
+            PublishMetric(new UntypedMetric(name: "total_count", metric: totalValueCount));
+
+            var suppressedValueRatio = (double)suppressedValueCount / totalValueCount;
+
+            var distinctValueCounts =
+                from row in distinctValuesQ.ResultRows
+                where !row.DistinctData.IsSuppressed
+                orderby row.Count descending
+                select new
+                {
+                    row.DistinctData.Value,
+                    row.Count,
+                };
+
+            PublishMetric(new UntypedMetric(name: "top_distinct_values", metric: distinctValueCounts.Take(10)));
+        }
+    }
+}
