42ccdfe1963b4329a87fbf867b474b0160b723dc
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-03-24 20:26:42 +0200

    added EmailColumnExplorer to check if a column contains emails by using the simple column trimming query

---
+++ src/explorer.api/Explorers/EmailColumnExplorer.cs
@@ -0,0 +1,46 @@
+namespace Explorer
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Explorer.Diffix.Extensions;
+    using Explorer.Queries;
+
+    internal class EmailColumnExplorer : ExplorerBase
+    {
+        public const string EmailAddressChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_.";
+
+        public EmailColumnExplorer(IQueryResolver queryResolver, string tableName, string columnName)
+            : base(queryResolver)
+        {
+            TableName = tableName;
+            ColumnName = columnName;
+        }
+
+        private string TableName { get; }
+
+        private string ColumnName { get; }
+
+        public override async Task Explore(CancellationToken cancellationToken)
+        {
+            var emailCheckQ = await ResolveQuery<TextColumnTrim.Result>(
+                new TextColumnTrim(TableName, ColumnName, TextColumnTrimType.Both, EmailAddressChars),
+                cancellationToken);
+
+            var (totalValueCount, suppressedValueCount) = emailCheckQ.ResultRows.CountTotalAndSuppressed();
+
+            var isEmail = totalValueCount == emailCheckQ.ResultRows
+                .Where(r => r.TrimmedText == "@")
+                .Sum(r => r.Count);
+
+            PublishMetric(new UntypedMetric(name: "is_email", metric: isEmail));
+
+            if (!isEmail)
+            {
+                return;
+            }
+        }
+    }
+}
