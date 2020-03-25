2c8a117c8c02629184ecfca2cc65e2094a8e48a5
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-03-25 11:26:07 +0200

    added tests for the TextColumnTrim query

---
+++ tests/explorer.api.tests/TextColumnTrimTests.cs
@@ -0,0 +1,54 @@
+namespace Explorer.Api.Tests
+{
+    using System;
+    using System.Linq;
+    using Explorer.Diffix.Extensions;
+    using Explorer.Queries;
+    using Xunit;
+
+    public sealed class TextColumnTrimTests : IClassFixture<TestWebAppFactory>
+    {
+        private const string TestDataSource = "gda_banking";
+
+        private readonly TestWebAppFactory factory;
+
+        public TextColumnTrimTests(TestWebAppFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async void TestEmailPositive()
+        {
+            var query = await factory.QueryResult(
+                new TextColumnTrim("clients", "email", TextColumnTrimType.Both, EmailColumnExplorer.EmailAddressChars),
+                TestDataSource,
+                nameof(TextColumnTrimTests));
+
+            var (totalValueCount, suppressedValueCount) = query.ResultRows.CountTotalAndSuppressed();
+
+            var isEmail = totalValueCount == query.ResultRows
+                .Where(r => r.TrimmedText == "@")
+                .Sum(r => r.Count);
+
+            Assert.True(isEmail);
+        }
+
+        [Fact]
+        public async void TestEmailNegative()
+        {
+            var query = await factory.QueryResult(
+                new TextColumnTrim("accounts", "lastname", TextColumnTrimType.Both, EmailColumnExplorer.EmailAddressChars),
+                TestDataSource,
+                nameof(TextColumnTrimTests));
+
+            var (totalValueCount, suppressedValueCount) = query.ResultRows.CountTotalAndSuppressed();
+
+            var isEmail = totalValueCount == query.ResultRows
+                .Where(r => r.TrimmedText == "@")
+                .Sum(r => r.Count);
+
+            Assert.False(isEmail);
+        }
+    }
+}
