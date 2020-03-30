a44a03fff5601989658e3fa6481a21a540f785fd
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-03-30 21:50:09 +0300

    added exploration of column prefixes

--- src/explorer.api/Explorers/TextColumnExplorer.cs
+++ src/explorer.api/Explorers/TextColumnExplorer.cs
@@ -1,6 +1,7 @@
 namespace Explorer
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace Explorer
 
     internal class TextColumnExplorer : ExplorerBase
     {
+        private const double SuppressedRatioThreshold = 0.1;
+
         public TextColumnExplorer(IQueryResolver queryResolver, string tableName, string columnName)
             : base(queryResolver)
         {
@@ -40,8 +43,6 @@ namespace Explorer
 
             PublishMetric(new UntypedMetric(name: "distinct.total_count", metric: counts.TotalCount));
 
-            var suppressedValueRatio = (double)suppressedValueCount / totalValueCount;
-
             var distinctValueCounts =
                 from row in distinctValuesQ.ResultRows
                 where row.DistinctData.HasValue
@@ -53,6 +54,69 @@ namespace Explorer
                 };
 
             PublishMetric(new UntypedMetric(name: "distinct.top_values", metric: distinctValueCounts.Take(10)));
+
+            if (counts.SuppressedCountRatio >= SuppressedRatioThreshold)
+            {
+                // we compute the common prefixes only if the row is not categorical
+                await ExplorePrefixes(cancellationToken);
+            }
+        }
+
+        private async Task<IEnumerable<Prefix>> ExplorePrefixes(CancellationToken cancellationToken)
+        {
+            var allPrefixes = new List<Prefix>();
+            var length = 0;
+            while (true)
+            {
+                length++;
+                var prefixesQ = await ResolveQuery<TextColumnPrefix.Result>(
+                    new TextColumnPrefix(TableName, ColumnName, length),
+                    cancellationToken);
+
+                var counts = prefixesQ.ResultRows.CountTotalAndSuppressed();
+                var avgCount = (double)counts.NonSuppressedCount / counts.NonSuppressedRows;
+
+                var prefixes =
+                    from row in prefixesQ.ResultRows
+                    let frequency = (double)row.Count / counts.NonSuppressedCount
+                    where row.HasValue && row.Count > avgCount
+                    orderby frequency descending
+                    select new Prefix(row.Prefix, frequency);
+
+                if (!prefixes.Any())
+                {
+                    break;
+                }
+
+                if (length > prefixes.Max(p => p.Value.Length))
+                {
+                    break;
+                }
+
+                allPrefixes.AddRange(prefixes);
+            }
+
+            var ret =
+                from row in allPrefixes
+                orderby row.Value.Length ascending, row.Frequency descending
+                select row;
+
+            PublishMetric(new UntypedMetric(name: "text.prefixes", metric: ret));
+
+            return ret;
+        }
+
+        private struct Prefix
+        {
+            public Prefix(string value, double frequency)
+            {
+                Value = value;
+                Frequency = frequency;
+            }
+
+            public string Value { get; }
+
+            public double Frequency { get; }
         }
     }
 }
