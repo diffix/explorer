b3ffc509efb4ac96d3547b659c4bbc6fcda69b79
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-05-04 16:28:54 +0000

    Temporary changes to the `TextLengthExplorer`
    1. Should have used `override` instead of `new` in `Explore()` method
    definition.
    
    2. The `TextLengthExplorer` crashed when used with a column that was
    considered 'isolating'. This change prevents this by checking the
    `show columns` query.
    
    3. Fixed usage of the metricNamePrefix.

--- src/explorer/Explorers/TextLengthExplorer.cs
+++ src/explorer/Explorers/TextLengthExplorer.cs
@@ -1,5 +1,7 @@
 namespace Explorer.Explorers
 {
+    using System.Linq;
+    using System.Text.Json;
     using System.Threading.Tasks;
 
     using Diffix;
@@ -7,15 +9,24 @@ namespace Explorer.Explorers
 
     internal class TextLengthExplorer : IntegerColumnExplorer
     {
-        private readonly string metricNamePrefix = "text.length";
+        private readonly string metricNamePrefix;
 
-        public TextLengthExplorer(string metricNamePrefix = "")
+        public TextLengthExplorer(string metricNamePrefix = "text.length")
         {
             this.metricNamePrefix = metricNamePrefix;
         }
 
-        public new async Task Explore(DConnection conn, ExplorerContext ctx)
+        public override async Task Explore(DConnection conn, ExplorerContext ctx)
         {
+            var isolators = await conn.Exec(new IsIsolatorColumn(ctx.Table));
+            var isIsolatorColumn = isolators.Rows.First(r => r.Item1 == ctx.Column).Item2;
+
+            if (isIsolatorColumn)
+            {
+                // TODO: Log the fact that we are aborting here.
+                return;
+            }
+
             var lengthContext = new ExplorerContext(ctx.Table, $"length({ctx.Column})", ctx.ColumnType);
 
             await base.Explore(conn, lengthContext);
@@ -28,6 +39,32 @@ namespace Explorer.Explorers
                 metric = new UntypedMetric(metricNamePrefix + "." + metric.Name, metric.Metric);
             }
             base.PublishMetric(metric);
+        }
+
+        // TODO: replace this with a component
+        private class IsIsolatorColumn : DQuery<(string, bool)>
+        {
+            public IsIsolatorColumn(string table)
+            {
+                QueryStatement = $"show columns from {table}";
+            }
+
+            public string QueryStatement { get; }
+
+            public (string, bool) ParseRow(ref Utf8JsonReader reader)
+            {
+                reader.Read();
+                var name = reader.GetString();
+
+                reader.Read(); // ignore data type
+
+                reader.Read();
+                var isolator = reader.GetString() == "true";
+
+                reader.Read(); // ignore key type
+
+                return (name, isolator);
+            }
         }
     }
 }
