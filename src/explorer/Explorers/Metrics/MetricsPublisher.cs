79868bea49e2ed05a9981bfad7b80ebc14d13db3
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-05-11 11:18:02 +0000

    Simplify MetricsPublisher again

--- src/explorer/Explorers/Metrics/MetricsPublisher.cs
+++ src/explorer/Explorers/Metrics/MetricsPublisher.cs
@@ -1,36 +1,24 @@
 namespace Explorer.Explorers.Metrics
 {
-    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
     public interface MetricsPublisher
     {
-        public bool PublishedMetrics(System.Guid id, out IEnumerable<ExploreMetric> metrics);
+        public void PublishMetric(ExploreMetric metric);
 
-        public IEnumerable<ExploreMetric> PublishedMetrics(System.Guid id)
-        {
-            if (PublishedMetrics(id, out var metrics))
-            {
-                return metrics;
-            }
-            throw new KeyNotFoundException($"No Metrics available for id {id}.");
-        }
-
-        public void PublishMetric(System.Guid id, ExploreMetric metric);
-
-        public void PublishMetrics(System.Guid id, MetricsProvider provider)
+        public void PublishMetrics(MetricsProvider provider)
         {
             foreach (var metric in provider.Metrics())
             {
-                PublishMetric(id, metric);
+                PublishMetric(metric);
             }
         }
 
-        public Task PublishMetricAsync(System.Guid id, ExploreMetric metric) =>
-            Task.Run(() => PublishMetric(id, metric));
+        public Task PublishMetricAsync(ExploreMetric metric) =>
+            Task.Run(() => PublishMetric(metric));
 
-        public Task PublishMetricsAsync(System.Guid id, MetricsProvider provider) =>
-            Task.WhenAll(provider.Metrics().Select(m => PublishMetricAsync(id, m)));
+        public Task PublishMetricsAsync(MetricsProvider provider) =>
+            Task.WhenAll(provider.Metrics().Select(PublishMetricAsync));
     }
 }
--- src/explorer/Explorers/Metrics/SimpleMetricsPublisher.cs
+++ src/explorer/Explorers/Metrics/SimpleMetricsPublisher.cs
@@ -1,36 +1,24 @@
 namespace Explorer.Explorers.Metrics
 {
     using System.Collections.Concurrent;
-    using System.Collections.Generic;
 
     using Microsoft.Extensions.Logging;
 
     public class SimpleMetricsPublisher : MetricsPublisher
     {
-        private readonly ConcurrentDictionary<System.Guid, ConcurrentDictionary<string, ExploreMetric>> store;
+        private readonly ConcurrentDictionary<string, ExploreMetric> store;
         private readonly ILogger<SimpleMetricsPublisher> logger;
 
         public SimpleMetricsPublisher(ILogger<SimpleMetricsPublisher> logger)
         {
             this.logger = logger;
-            store = new ConcurrentDictionary<System.Guid, ConcurrentDictionary<string, ExploreMetric>>();
+            store = new ConcurrentDictionary<string, ExploreMetric>();
         }
 
-        public bool PublishedMetrics(System.Guid id, out IEnumerable<ExploreMetric> metrics)
-        {
-            if (store.TryGetValue(id, out var metricsDict))
-            {
-                metrics = metricsDict.Values;
-                return true;
-            }
-            metrics = System.Array.Empty<ExploreMetric>();
-            return false;
-        }
-
-        public void PublishMetric(System.Guid id, ExploreMetric metric)
+        public void PublishMetric(ExploreMetric metric)
         {
             logger.LogDebug($"{nameof(SimpleMetricsPublisher)}: Publishing metric {metric.Name}.");
-            store[id][metric.Name] = metric;
+            store[metric.Name] = metric;
         }
     }
 }
