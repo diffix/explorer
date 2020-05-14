75b52e54de93358d8da427e23e28693224a3e979
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-05-14 15:22:36 +0000

    Add builder classes for Context and Connection
    Encapsulates the logic for building these objects rather than building
    them inline in the controller. Also plays more nicely with dependency
    injection.

---
+++ src/explorer.api/Exploration/Setup/ContextBuilder.cs
@@ -0,0 +1,60 @@
+﻿namespace Explorer.Api
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Aircloak.JsonApi;
+    using Diffix;
+    using Explorer.Common;
+
+    internal class ContextBuilder
+    {
+        private readonly JsonApiClient apiClient;
+
+        public ContextBuilder(JsonApiClient apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        public async Task<ExplorerContext> Build(Models.ExploreParams data)
+        {
+            var dataSources = await apiClient.GetDataSources(CancellationToken.None);
+
+            if (!dataSources.AsDict.TryGetValue(data.DataSourceName, out var exploreDataSource))
+            {
+                throw new MetaDataCheckException($"Could not find datasource '{data.DataSourceName}'.");
+            }
+
+            if (!exploreDataSource.TableDict.TryGetValue(data.TableName, out var exploreTableMeta))
+            {
+                throw new MetaDataCheckException($"Could not find table '{data.TableName}'.");
+            }
+
+            if (!exploreTableMeta.ColumnDict.TryGetValue(data.ColumnName, out var exploreColumnMeta))
+            {
+                throw new MetaDataCheckException($"Could not find column '{data.ColumnName}'.");
+            }
+
+            return new CheckedContext(data, exploreColumnMeta.Type);
+        }
+
+        internal class CheckedContext : ExplorerContext
+        {
+            internal CheckedContext(Models.ExploreParams data, DValueType columnType)
+            {
+                DataSource = data.DataSourceName;
+                Table = data.TableName;
+                Column = data.ColumnName;
+                ColumnType = columnType;
+            }
+
+            public string DataSource { get; }
+
+            public string Table { get; }
+
+            public string Column { get; }
+
+            public DValueType ColumnType { get; }
+        }
+    }
+}
