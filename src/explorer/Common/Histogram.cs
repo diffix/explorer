f3328d1d099742c1d304f12f5a07cda9818df9d1
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-05-18 13:33:45 +0000

    Add more strongly-typed histograms.

---
+++ src/explorer/Common/Histogram.cs
@@ -0,0 +1,38 @@
+namespace Explorer.Common
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    using Explorer.Queries;
+
+    public class Histogram
+    {
+        public Histogram(BucketSize bucketSize, IEnumerable<HistogramBucket> buckets)
+        {
+            Debug.Assert(
+                buckets.All(b => b.BucketSize.SnappedSize == bucketSize.SnappedSize),
+                $"Histogram buckets don't match given bucketsize");
+
+            BucketSize = bucketSize;
+            Buckets = new SortedList<decimal, HistogramBucket>(buckets.ToDictionary(b => b.LowerBound));
+        }
+
+        public BucketSize BucketSize { get; }
+
+        public SortedList<decimal, HistogramBucket> Buckets { get; }
+
+        public static IEnumerable<Histogram> FromQueryRows(IEnumerable<SingleColumnHistogram.Result> queryResults) =>
+            from row in queryResults
+            where row.HasValue
+            group row by row.BucketSize into bucketGroup
+            let bucketSize = new BucketSize(bucketGroup.Key)
+            select new Histogram(
+                bucketSize,
+                bucketGroup.Select(b =>
+                    new HistogramBucket(
+                        (decimal)b.LowerBound,
+                        bucketSize,
+                        NoisyCount.FromCountableRow(b))));
+    }
+}
