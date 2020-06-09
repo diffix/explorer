51a3668f82e002db603376b60e77501df534e4b2
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-06-09 14:04:17 +0000

    Remove ApiUrl from config

--- src/explorer.api/ExplorerConfig.cs
+++ src/explorer.api/ExplorerConfig.cs
@@ -4,31 +4,10 @@ namespace Explorer.Api
 
     public class ExplorerConfig
     {
-        public const string AircloakApiUrlEnvironmentVariable = "AIRCLOAK_API_URL";
-
         public const string ApiKeyEnvironmentVariable = "AIRCLOAK_API_KEY";
-
-        public Uri? AircloakApiUrlDefault { get; set; }
 
         public uint PollFrequency { get; set; } = 2000;
 
         public TimeSpan PollFrequencyTimeSpan => TimeSpan.FromMilliseconds(PollFrequency);
-
-        public Uri AircloakApiUrl()
-        {
-            var fromEnv =
-                Environment.GetEnvironmentVariable(AircloakApiUrlEnvironmentVariable);
-
-            if (!string.IsNullOrEmpty(fromEnv))
-            {
-                // If there is no trailing slash, add one, otherwise it messes up api request routing
-                return new Uri(fromEnv.EndsWith('/') ? fromEnv : fromEnv + '/');
-            }
-
-            return AircloakApiUrlDefault ??
-                    throw new Exception(
-                        $"Aircloak Api Url needs to be set either using the {AircloakApiUrlEnvironmentVariable} " +
-                        $"environment variable or the {nameof(AircloakApiUrlDefault)} config item.");
-        }
     }
 }
