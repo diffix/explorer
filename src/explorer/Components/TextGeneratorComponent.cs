diffix/explorer

commit 87f130da678f6b8bb26cefcd5944283d13a9a6e0

Move TextUtilities into
The static  methods defined in `TextUtilities` were only ever used in
`TextGeneratorComponent` so moved them there.

Created a `Constants` file too, to hold constants. We can add more as
needed.

src/explorer/Components/TextGeneratorComponent.cs
@@ -3,6 +3,7 @@ namespace Explorer.Components
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading.Tasks;
 
     using Diffix;
@@ -60,7 +61,7 @@ namespace Explorer.Components
         private static async Task<SubstringWithCountList> ExploreEmailDomains(DConnection conn, ExplorerContext ctx)
         {
             var domains = await conn.Exec(new TextColumnTrim(
-                ctx.Table, ctx.Column, TextColumnTrimType.Leading, TextUtilities.EmailAddressChars));
+                ctx.Table, ctx.Column, TextColumnTrimType.Leading, Constants.EmailAddressChars));
 
             return SubstringWithCountList.FromValueWithCountEnum(
                 domains.Rows
@@ -96,6 +97,126 @@ namespace Explorer.Components
             var rand = new Random(Environment.TickCount);
             return Enumerable.Range(0, GeneratedValuesCount).Select(_
                 => TextUtilities.GenerateString(substrings, minLength: 3, rand));
+        }
+
+        private static string GenerateString(SubstringsData substrings, int minLength, Random rand)
+        {
+            var sb = new StringBuilder();
+            var len = rand.Next(minLength, substrings.Count);
+            for (var pos = 0; pos < substrings.Count && sb.Length < len; pos++)
+            {
+                var str = substrings.GetRandomSubstring(pos, rand);
+                sb.Append(str);
+                pos += str.Length;
+            }
+            return sb.ToString();
+        }
+
+        private static string GenerateEmail(
+            SubstringsData substrings,
+            SubstringWithCountList domains,
+            SubstringWithCountList tlds,
+            Random rand,
+            int domainsCountThreshold)
+        {
+            // create local-part section
+            var str = GenerateString(substrings, minLength: 6, rand);
+            var allParts = str.Split('@', StringSplitOptions.RemoveEmptyEntries);
+            var sb = new StringBuilder();
+            var partIndex = 0;
+            var pnext = 1;
+            while (partIndex < allParts.Length && rand.NextDouble() <= pnext)
+            {
+                sb.Append(allParts[partIndex]);
+                pnext /= 2;
+                partIndex++;
+            }
+            var localParts = sb.ToString()
+                .Split('.', StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => s.Length == 1 || s.Length > 3)
+                .Take(rand.Next(1, 3));
+            var localPart = string.Join('.', localParts);
+            if (string.IsNullOrEmpty(localPart))
+            {
+                return string.Empty;
+            }
+            if (domains.Count >= domainsCountThreshold)
+            {
+                // if the number of distinct domains is big enough we select one from the extracted list
+                return localPart + domains.GetRandomValue(rand, @default: string.Empty);
+            }
+
+            // create domain section
+            sb.Clear();
+            while (partIndex < allParts.Length)
+            {
+                sb.Append(allParts[partIndex]);
+                partIndex++;
+            }
+            var domainParts = sb.ToString()
+                .Split('.', StringSplitOptions.RemoveEmptyEntries)
+                .Where(p => p.Length > 3);
+            var domain = rand.NextDouble() > 0.15 ?
+                domainParts.Aggregate(string.Empty, (max, cur) => max.Length > cur.Length ? max : cur) :
+                string.Join('.', domainParts);
+            if (string.IsNullOrEmpty(domain) || domain.Length < 4)
+            {
+                return string.Empty;
+            }
+            return localPart + "@" + domain + tlds.GetRandomValue(rand, @default: string.Empty);
+        }
+
+        private static IEnumerable<string> GenerateEmails(
+            SubstringsData substrings,
+            SubstringWithCountList domains,
+            SubstringWithCountList tlds,
+            int generatedValuesCount,
+            int domainsCountThreshold)
+        {
+            var rand = new Random(Environment.TickCount);
+            var emails = new List<string>(generatedValuesCount);
+            for (var i = 0; emails.Count < generatedValuesCount && i < generatedValuesCount * 100; i++)
+            {
+                var email = GenerateEmail(substrings, domains, tlds, rand, domainsCountThreshold);
+                if (!string.IsNullOrEmpty(email))
+                {
+                    emails.Add(email);
+                }
+            }
+            return emails;
+        }
+
+        /// <summary>
+        /// Finds common substrings for each position in the texts of the specified column.
+        /// It uses a batch approach to query for several positions (specified using SubstringQueryColumnCount)
+        /// using a single query.
+        /// </summary>
+        private static async Task<SubstringsData> ExploreSubstrings(
+            DConnection conn,
+            ExplorerContext ctx,
+            int substringQueryColumnCount,
+            params int[] substringLengths)
+        {
+            var substrings = new SubstringsData();
+            foreach (var length in substringLengths)
+            {
+                var hasRows = true;
+                for (var pos = 0; hasRows; pos += substringQueryColumnCount)
+                {
+                    var query = new TextColumnSubstring(ctx.Table, ctx.Column, pos, length, substringQueryColumnCount);
+                    var sstrResult = await conn.Exec(query);
+                    hasRows = false;
+                    foreach (var row in sstrResult.Rows)
+                    {
+                        if (row.HasValue)
+                        {
+                            hasRows = true;
+                            substrings.Add(pos + row.Index, row.Value, row.Count);
+                        }
+                    }
+                }
+            }
+            return substrings;
         }
     }
 }

src/explorer/Common/Constants.cs
@@ -1,0 +1,7 @@
+﻿namespace Explorer.Common
+{
+    public static class Constants
+    {
+        public const string EmailAddressChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_.";
+    }
+}

src/explorer/Common/TextUtilities.cs
@@ -1,138 +1,0 @@
-namespace Explorer.Common
-{
-
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Diffix;
-    using Explorer.Queries;
-
-    using SubstringWithCountList = ValueWithCountList<string>;
-
-    public static class TextUtilities
-    {
-        public const string EmailAddressChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_.";
-
-        internal static string GenerateString(SubstringsData substrings, int minLength, Random rand)
-        {
-            var sb = new StringBuilder();
-            var len = rand.Next(minLength, substrings.Count);
-            for (var pos = 0; pos < substrings.Count && sb.Length < len; pos++)
-            {
-                var str = substrings.GetRandomSubstring(pos, rand);
-                sb.Append(str);
-                pos += str.Length;
-            }
-            return sb.ToString();
-        }
-
-        internal static string GenerateEmail(
-            SubstringsData substrings,
-            SubstringWithCountList domains,
-            SubstringWithCountList tlds,
-            Random rand,
-            int domainsCountThreshold)
-        {
-            // create local-part section
-            var str = GenerateString(substrings, minLength: 6, rand);
-            var allParts = str.Split('@', StringSplitOptions.RemoveEmptyEntries);
-            var sb = new StringBuilder();
-            var partIndex = 0;
-            var pnext = 1;
-            while (partIndex < allParts.Length && rand.NextDouble() <= pnext)
-            {
-                sb.Append(allParts[partIndex]);
-                pnext /= 2;
-                partIndex++;
-            }
-            var localParts = sb.ToString()
-                .Split('.', StringSplitOptions.RemoveEmptyEntries)
-                .Where(s => s.Length == 1 || s.Length > 3)
-                .Take(rand.Next(1, 3));
-            var localPart = string.Join('.', localParts);
-            if (string.IsNullOrEmpty(localPart))
-            {
-                return string.Empty;
-            }
-            if (domains.Count >= domainsCountThreshold)
-            {
-                // if the number of distinct domains is big enough we select one from the extracted list
-                return localPart + domains.GetRandomValue(rand, @default: string.Empty);
-            }
-
-            // create domain section
-            sb.Clear();
-            while (partIndex < allParts.Length)
-            {
-                sb.Append(allParts[partIndex]);
-                partIndex++;
-            }
-            var domainParts = sb.ToString()
-                .Split('.', StringSplitOptions.RemoveEmptyEntries)
-                .Where(p => p.Length > 3);
-            var domain = rand.NextDouble() > 0.15 ?
-                domainParts.Aggregate(string.Empty, (max, cur) => max.Length > cur.Length ? max : cur) :
-                string.Join('.', domainParts);
-            if (string.IsNullOrEmpty(domain) || domain.Length < 4)
-            {
-                return string.Empty;
-            }
-            return localPart + "@" + domain + tlds.GetRandomValue(rand, @default: string.Empty);
-        }
-
-        internal static IEnumerable<string> GenerateEmails(
-            SubstringsData substrings,
-            SubstringWithCountList domains,
-            SubstringWithCountList tlds,
-            int generatedValuesCount,
-            int domainsCountThreshold)
-        {
-            var rand = new Random(Environment.TickCount);
-            var emails = new List<string>(generatedValuesCount);
-            for (var i = 0; emails.Count < generatedValuesCount && i < generatedValuesCount * 100; i++)
-            {
-                var email = GenerateEmail(substrings, domains, tlds, rand, domainsCountThreshold);
-                if (!string.IsNullOrEmpty(email))
-                {
-                    emails.Add(email);
-                }
-            }
-            return emails;
-        }
-
-        /// <summary>
-        /// Finds common substrings for each position in the texts of the specified column.
-        /// It uses a batch approach to query for several positions (specified using SubstringQueryColumnCount)
-        /// using a single query.
-        /// </summary>
-        internal static async Task<SubstringsData> ExploreSubstrings(
-            DConnection conn,
-            ExplorerContext ctx,
-            int substringQueryColumnCount,
-            params int[] substringLengths)
-        {
-            var substrings = new SubstringsData();
-            foreach (var length in substringLengths)
-            {
-                var hasRows = true;
-                for (var pos = 0; hasRows; pos += substringQueryColumnCount)
-                {
-                    var query = new TextColumnSubstring(ctx.Table, ctx.Column, pos, length, substringQueryColumnCount);
-                    var sstrResult = await conn.Exec(query);
-                    hasRows = false;
-                    foreach (var row in sstrResult.Rows)
-                    {
-                        if (row.HasValue)
-                        {
-                            hasRows = true;
-                            substrings.Add(pos + row.Index, row.Value, row.Count);
-                        }
-                    }
-                }
-            }
-            return substrings;
-        }
-    }
-}

src/explorer/Components/EmailCheckComponent.cs
@@ -30,7 +30,7 @@ namespace Explorer.Components
         private static async Task<bool> CheckIsEmail(DConnection conn, ExplorerContext ctx)
         {
             var emailCheck = await conn.Exec(
-                new TextColumnTrim(ctx.Table, ctx.Column, TextColumnTrimType.Both, TextUtilities.EmailAddressChars));
+                new TextColumnTrim(ctx.Table, ctx.Column, TextColumnTrimType.Both, Constants.EmailAddressChars));
 
             return emailCheck.Rows.All(r => r.IsNull || (!r.IsSuppressed && r.Value == "@"));
         }

tests/explorer.tests/TextColumnTrimTests.cs
@@ -23,7 +23,7 @@ namespace Explorer.Tests
                 VcrSharp.Cassette.GenerateVcrFilename(this));
 
             var result = await queryScope.QueryRows(
-                new TextColumnTrim("clients", "email", TextColumnTrimType.Both, TextUtilities.EmailAddressChars));
+                new TextColumnTrim("clients", "email", TextColumnTrimType.Both, Constants.EmailAddressChars));
 
             var counts = ValueCounts.Compute(result);
 
@@ -42,7 +42,7 @@ namespace Explorer.Tests
                 VcrSharp.Cassette.GenerateVcrFilename(this));
 
             var result = await queryScope.QueryRows(
-                new TextColumnTrim("cards", "lastname", TextColumnTrimType.Both, TextUtilities.EmailAddressChars));
+                new TextColumnTrim("cards", "lastname", TextColumnTrimType.Both, Constants.EmailAddressChars));
 
             var counts = ValueCounts.Compute(result);
 

commit 40edd8ee71f6e90e76ec29c002961293f60a56fd

Fix a mistake from previous commit
commit 87f130d introduced a compile error, this fixes it.

src/explorer/Components/TextGeneratorComponent.cs
@@ -80,23 +80,23 @@ namespace Explorer.Components
         private async Task<IEnumerable<string>> GenerateEmails()
         {
             var (substrings, domains, tlds) = await Utilities.WhenAll(
-                TextUtilities.ExploreSubstrings(
+                ExploreSubstrings(
                     conn, ctx, SubstringQueryColumnCount, substringLengths: new int[] { 3, 4 }),
                 ExploreEmailDomains(conn, ctx),
                 ExploreEmailTopLevelDomains(conn, ctx));
 
-            return await Task.Run(() => TextUtilities.GenerateEmails(
+            return await Task.Run(() => GenerateEmails(
                     substrings, domains, tlds, GeneratedValuesCount, EmailDomainsCountThreshold));
         }
 
         private async Task<IEnumerable<string>> GenerateStrings()
         {
             // the substring lengths 3 and 4 were determined empirically to work for column containing names
-            var substrings = await TextUtilities.ExploreSubstrings(
+            var substrings = await ExploreSubstrings(
                 conn, ctx, SubstringQueryColumnCount, substringLengths: new int[] { 3, 4 });
             var rand = new Random(Environment.TickCount);
             return Enumerable.Range(0, GeneratedValuesCount).Select(_
-                => TextUtilities.GenerateString(substrings, minLength: 3, rand));
+                => GenerateString(substrings, minLength: 3, rand));
         }
 
         private static string GenerateString(SubstringsData substrings, int minLength, Random rand)