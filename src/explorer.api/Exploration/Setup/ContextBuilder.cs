9b5d9281f6a30635514a63baf8ee137009563ade
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-06-17 10:37:22 +0000

    Simple caching scheme for DataSourceCollection
    DataSourceCollection can be quite large takes a non-trivial amount of
    time to parse from json. This change adds a ConcurrentDictionary to
    the ContextBuilder to cache previously received DataSourceCollections.

--- src/explorer.api/Exploration/Setup/ContextBuilder.cs
+++ src/explorer.api/Exploration/Setup/ContextBuilder.cs
@@ -1,14 +1,18 @@
 ﻿namespace Explorer.Api
 {
-    using System.Threading;
+    using System.Collections.Concurrent;
     using System.Threading.Tasks;
 
     using Aircloak.JsonApi;
+    using Aircloak.JsonApi.ResponseTypes;
     using Diffix;
     using Explorer.Common;
 
     public class ContextBuilder
     {
+        private static readonly ConcurrentDictionary<string, DataSourceCollection> DataSourcesCache
+            = new ConcurrentDictionary<string, DataSourceCollection>();
+
         private readonly JsonApiClient apiClient;
 
         public ContextBuilder(JsonApiClient apiClient)
@@ -18,8 +22,25 @@
 
         public async Task<ExplorerContext> Build(Models.ExploreParams data)
         {
-            var dataSources = await apiClient.GetDataSources(new System.Uri(data.ApiUrl), CancellationToken.None);
+            if (DataSourcesCache.TryGetValue(data.ApiUrl, out var dataSources))
+            {
+                try
+                {
+                    return CheckAndBuild(data, dataSources);
+                }
+                catch (MetaDataCheckException)
+                {
+                    // Validation failed with cached data sources, swallow this exception and try with fresh data.
+                }
+            }
 
+            DataSourcesCache[data.ApiUrl] = await apiClient.GetDataSources(new System.Uri(data.ApiUrl), default);
+
+            return CheckAndBuild(data, DataSourcesCache[data.ApiUrl]);
+        }
+
+        private static ExplorerContext CheckAndBuild(Models.ExploreParams data, DataSourceCollection dataSources)
+        {
             if (!dataSources.AsDict.TryGetValue(data.DataSourceName, out var exploreDataSource))
             {
                 throw new MetaDataCheckException($"Could not find datasource '{data.DataSourceName}'.");
