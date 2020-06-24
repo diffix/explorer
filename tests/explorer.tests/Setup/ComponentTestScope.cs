edbc781216a6799c25ec0654c6e17082860eea01
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-06-24 21:31:59 +0000

    Resolve an issue with resolution of publishers
    This became apparent when adding the component test
    `TestDistinctValuesMetricContainsRemainder`.
    It appears that PublisherComponents are registered against the interface
    and so therefore if you request the concrete class through DI, you get an
    instance with transient lifetime instead of scoped (which is desired).
    This has been fixed by adding an extension method to request the
    appropriate PublisherComponent through its interface using Lamar's
    name resolution convention.
    
    (If that sounds cryptic, hopefully the code change in `LamarExtensions` is
    somewhat clearer.

--- tests/explorer.tests/Setup/ComponentTestScope.cs
+++ tests/explorer.tests/Setup/ComponentTestScope.cs
@@ -6,6 +6,8 @@ namespace Explorer.Tests
     using Explorer.Components;
     using Explorer.Metrics;
 
+    using Xunit;
+
     public class ComponentTestScope : QueryableTestScope
     {
         public ComponentTestScope(TestScope testScope)
@@ -13,27 +15,37 @@ namespace Explorer.Tests
         {
         }
 
-        public async Task Test<TComponent, TResult>(Action<TResult> test)
-        where TComponent : ExplorerComponent<TResult>
+        public async Task ResultTest<TComponent, TResult>(Action<TResult> test)
+        where TComponent : ResultProvider<TResult>
         {
-            var c = Inner.Scope.GetInstance<TComponent>();
+            // Resolve the component using the interface to ensure correct scope
+            var c = Inner.Scope.GetInstance<ResultProvider<TResult>>();
+            Assert.IsType<TComponent>(c);
+
             var result = await c.ResultAsync;
 
             test(result);
         }
 
-        public async Task Test<T>(Action<IEnumerable<ExploreMetric>> test)
+        public async Task MetricsTest<T>(Action<IEnumerable<ExploreMetric>> test)
         where T : PublisherComponent
         {
-            var p = Inner.Scope.GetInstance<T>();
+            var publisher = Inner.Scope.ResolvePublisherComponent<T>();
 
             var metrics = new List<ExploreMetric>();
-            await foreach (var m in p.YieldMetrics())
+            await foreach (var m in publisher.YieldMetrics())
             {
                 metrics.Add(m);
             }
 
             test(metrics);
+        }
+
+        public void ConfigurePublisher<T>(Action<T> doSomething)
+        where T : PublisherComponent
+        {
+            var publisher = Inner.Scope.ResolvePublisherComponent<T>();
+            doSomething(publisher);
         }
     }
 }
