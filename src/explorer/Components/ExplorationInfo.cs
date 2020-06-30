f1d8e87ed39eac51372df1af2bc7d83e396c2efb
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-06-30 12:14:23 +0000

    Simplify ExplorationInfo
    ExplorationInfo doesn't need to implement ExplorerComponent.

--- src/explorer/Components/ExplorationInfo.cs
+++ src/explorer/Components/ExplorationInfo.cs
@@ -1,13 +1,11 @@
 namespace Explorer.Components
 {
     using System.Collections.Generic;
-    using System.Threading.Tasks;
 
     using Explorer.Common;
     using Explorer.Metrics;
 
-    public class ExplorationInfo
-        : ExplorerComponent<ExplorationInfo.Result>, PublisherComponent
+    public class ExplorationInfo : PublisherComponent
     {
         private readonly ExplorerContext ctx;
 
@@ -18,34 +16,12 @@ namespace Explorer.Components
 
         public async IAsyncEnumerable<ExploreMetric> YieldMetrics()
         {
-            var r = await ResultAsync;
             yield return new UntypedMetric("exploration_info", new
             {
-                r.DataSource,
-                r.Table,
-                r.Column,
+                ctx.DataSource,
+                ctx.Table,
+                ctx.Column,
             });
-        }
-
-        protected override Task<Result> Explore()
-        {
-            return Task.FromResult(new Result(ctx.DataSource, ctx.Table, ctx.Column));
-        }
-
-        public class Result
-        {
-            public Result(string dataSource, string table, string column)
-            {
-                DataSource = dataSource;
-                Table = table;
-                Column = column;
-            }
-
-            public string DataSource { get; }
-
-            public string Table { get; }
-
-            public string Column { get; }
         }
     }
 }
