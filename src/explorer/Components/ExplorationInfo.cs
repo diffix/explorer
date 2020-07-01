7ecb75783035e9dd303398ca9d607f2866c0679b
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-07-01 12:12:32 +0000

    Fix conversion of ColumnType in ExplorationInfo

--- src/explorer/Components/ExplorationInfo.cs
+++ src/explorer/Components/ExplorationInfo.cs
@@ -1,7 +1,10 @@
 namespace Explorer.Components
 {
     using System.Collections.Generic;
+    using System.Text.Json.Serialization;
 
+    using Diffix;
+    using Diffix.JsonConversion;
     using Explorer.Common;
     using Explorer.Metrics;
 
@@ -14,15 +17,18 @@ namespace Explorer.Components
             this.ctx = ctx;
         }
 
+        public string DataSource { get => ctx.DataSource; }
+
+        public string Table { get => ctx.Table; }
+
+        public string Column { get => ctx.Column; }
+
+        [JsonConverter(typeof(DValueTypeEnumConverter))]
+        public DValueType ColumnType { get => ctx.ColumnType; }
+
         public async IAsyncEnumerable<ExploreMetric> YieldMetrics()
         {
-            yield return new UntypedMetric("exploration_info", new
-            {
-                ctx.DataSource,
-                ctx.Table,
-                ctx.Column,
-                ctx.ColumnType,
-            });
+            yield return new UntypedMetric("exploration_info", this);
         }
     }
 }
