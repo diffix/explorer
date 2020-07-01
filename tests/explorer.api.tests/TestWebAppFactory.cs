65e0bc8a0bde2b6a8c4b52984438d46818bb03ff
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-07-01 13:22:23 +0000

    Use correct path for appsetting.Test.json
    Also, change test log levels to 'Warn'

--- tests/explorer.api.tests/TestWebAppFactory.cs
+++ tests/explorer.api.tests/TestWebAppFactory.cs
@@ -11,6 +11,7 @@
     using Aircloak.JsonApi;
     using Explorer.Api;
     using Explorer.Api.Authentication;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc.Testing;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -18,7 +19,7 @@
     public class TestWebAppFactory : WebApplicationFactory<Startup>
     {
         public static readonly ExplorerConfig Config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.Test.json")
+            .AddJsonFile($"{Environment.CurrentDirectory}/../../../../appsettings.Test.json")
             .AddEnvironmentVariables()
             .Build()
             .GetSection("Explorer")
@@ -90,7 +91,7 @@
         }
 #pragma warning restore CA1822 // method should be made static
 
-        protected override void ConfigureWebHost(Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             var testConfig = GetTestConfig(GetType().Name, "WebHost.OutgoingRequests");
 
