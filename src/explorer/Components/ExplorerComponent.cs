8d8e1e56622f8d57c7c9bc0487d68ea3b1d2e00e
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-07-09 14:30:36 +0300

    Added the class constraint the to the generic type parameter in ExplorerComponent This will avoid the use of primitive types as the type argument for ExplorerComponent<>. The type arg should actually be something unique to the component.

--- src/explorer/Components/ExplorerComponent.cs
+++ src/explorer/Components/ExplorerComponent.cs
@@ -3,6 +3,7 @@ namespace Explorer.Components
     using System.Threading.Tasks;
 
     public abstract class ExplorerComponent<TResult> : ResultProvider<TResult>
+    where TResult : class
     {
         private Task<TResult>? componentTask;
 
