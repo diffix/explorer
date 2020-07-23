4a8e3360e9bf6b30499cd115fec821cc30c1a592
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-07-23 07:04:38 +0000

    Add docstrings to ExplorationStatus

--- src/explorer/ExplorationStatus.cs
+++ src/explorer/ExplorationStatus.cs
@@ -2,15 +2,32 @@ namespace Explorer
 {
     using System.Text.Json.Serialization;
 
-#pragma warning disable SA1602 // Enumeration items should be documented
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ExplorationStatus
     {
+        /// <summary>
+        /// Waiting to be run.
+        /// </summary>
         New,
+
+        /// <summary>
+        /// Running.
+        /// </summary>
         Processing,
+
+        /// <summary>
+        /// Completed Successfully.
+        /// </summary>
         Complete,
+
+        /// <summary>
+        /// Completed due due cancellation.
+        /// </summary>
         Canceled,
+
+        /// <summary>
+        /// Completed with errors.
+        /// </summary>
         Error,
     }
-#pragma warning restore
 }
