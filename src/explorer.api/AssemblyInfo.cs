75396885b825d011cb9bf4c01b9cceb20381d14f
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-07-29 09:21:09 +0000

    Add basic version info to the assembly
    This can be picked up and propagated to sentry.

--- src/explorer.api/AssemblyInfo.cs
+++ src/explorer.api/AssemblyInfo.cs
@@ -1,1 +1,7 @@
+using System.Reflection;
+
+// Set version number for the assembly.
+[assembly: AssemblyVersion(
+    ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + ".0")]
+[assembly: AssemblyInformationalVersion(ThisAssembly.Git.Tag)]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("explorer.api.tests")]
