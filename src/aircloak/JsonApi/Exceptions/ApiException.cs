3775e12e0c81e35b3c32e7c1a60670c03334c6a5
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-07-31 18:17:50 +0300

    fill XML comments for Aircloak API exceptions

--- src/aircloak/JsonApi/Exceptions/ApiException.cs
+++ src/aircloak/JsonApi/Exceptions/ApiException.cs
@@ -1,13 +1,47 @@
 namespace Aircloak.JsonApi.Exceptions
 {
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Represents errors that occur during JSON API requests.
+    /// </summary>
     [System.Serializable]
     public class ApiException : System.Exception
     {
-        public ApiException() { }
-        public ApiException(string message) : base(message) { }
-        public ApiException(string message, System.Exception inner) : base(message, inner) { }
-        protected ApiException(
-            System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException" /> class.
+        /// </summary>
+        public ApiException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public ApiException(string message)
+        : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public ApiException(string message, System.Exception inner)
+        : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException" /> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
+        protected ApiException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+        {
+        }
     }
 }
--- src/aircloak/JsonApi/Exceptions/ResultException.cs
+++ src/aircloak/JsonApi/Exceptions/ResultException.cs
@@ -1,13 +1,47 @@
 namespace Aircloak.JsonApi.Exceptions
 {
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Represents errors signaling an API result with "Error" status.
+    /// </summary>
     [System.Serializable]
     public class ResultException : ApiException
     {
-        public ResultException() { }
-        public ResultException(string message) : base(message) { }
-        public ResultException(string message, System.Exception inner) : base(message, inner) { }
-        protected ResultException(
-            System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResultException" /> class.
+        /// </summary>
+        public ResultException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResultException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public ResultException(string message)
+        : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResultException" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public ResultException(string message, System.Exception inner)
+        : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResultException" /> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
+        protected ResultException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+        {
+        }
     }
 }
