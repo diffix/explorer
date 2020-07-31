d903a9af8cbf1fda2d804babb9a14ece1f564b76
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-07-31 10:36:20 +0300

    combined all test scope utils in TestScope class

--- tests/explorer.tests/Setup/TestScope.cs
+++ tests/explorer.tests/Setup/TestScope.cs
@@ -1,60 +1,109 @@
 namespace Explorer.Tests
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading;
+    using System.Threading.Tasks;
 
     using Aircloak.JsonApi;
     using Diffix;
+    using Explorer.Components;
+    using Explorer.Metrics;
     using Lamar;
     using VcrSharp;
+    using Xunit;
 
     public class TestScope : IDisposable
     {
         private bool disposedValue;
 
-        public TestScope(Container rootContainer)
-        {
-            Scope = rootContainer.GetNestedContainer();
-#pragma warning disable CA2000 // Call System.IDisposable.Dispose on object (Object lifetime is managed by container.)
-            Scope.InjectDisposable(new CancellationTokenSource());
-#pragma warning restore CA2000 // Call System.IDisposable.Dispose on object
-        }
-
-        public INestedContainer Scope { get; }
-
-        public TestScope LoadCassette(string testFileName)
-        {
-#pragma warning disable CA2000 // Call System.IDisposable.Dispose on object (Object lifetime is managed by container.)
-            Scope.InjectDisposable(new Cassette($"../../../.vcr/{testFileName}.yaml"));
-#pragma warning restore CA2000 // Call System.IDisposable.Dispose on object
-
-            return this;
-        }
-
-        public TestScope OverrideVcrOptions(
+        public TestScope(
+            Container rootContainer,
+            Uri apiUri,
+            string dataSource,
+            string table,
+            string column,
+            DColumnInfo columnInfo,
+            string vcrFileName,
             VCRMode vcrMode = VCRMode.Cache,
-            RecordingOptions recordingOptions = RecordingOptions.SuccessOnly)
+            RecordingOptions recordingOptions = RecordingOptions.SuccessOnly,
+            int pollFrequencySecs = 2)
         {
+#pragma warning disable CA2000 // Call System.IDisposable.Dispose on object (lifetime is managed by container.)
+            var cts = new CancellationTokenSource();
+            var cassette = new Cassette($"../../../.vcr/{vcrFileName}.yaml");
+#pragma warning restore CA2000 // Call System.IDisposable.Dispose on object
+
+            Scope = rootContainer.GetNestedContainer();
+
+            Connection = new AircloakConnection(
+                Scope.GetInstance<JsonApiClient>(),
+                apiUri,
+                dataSource,
+                TimeSpan.FromSeconds(pollFrequencySecs),
+                cts.Token);
+
+            Context = new ExplorerTestContext(Connection, dataSource, table, column, columnInfo);
+
             var vcrFactory = Scope.GetInstance<VcrApiHttpClientFactory>();
             vcrFactory.VcrMode = vcrMode;
             vcrFactory.RecordingOptions = recordingOptions;
 
-            return this;
+            Scope.Inject<ExplorerContext>(Context);
+            Scope.InjectDisposable(cts);
+            Scope.InjectDisposable(cassette);
         }
 
-        public QueryableTestScope WithConnectionParams(
-            Uri apiUrl,
-            string dataSourceName,
-            int pollFrequencySecs = 2)
+        public ExplorerTestContext Context { get; }
+
+        private INestedContainer Scope { get; }
+
+        private AircloakConnection Connection { get; }
+
+        public async Task<IEnumerable<TRow>> QueryRows<TRow>(DQuery<TRow> query)
         {
-            Scope.Inject<DConnection>(
-                new AircloakConnection(
-                Scope.GetInstance<JsonApiClient>(),
-                apiUrl,
-                dataSourceName,
-                TimeSpan.FromSeconds(pollFrequencySecs),
-                Scope.GetInstance<CancellationTokenSource>().Token));
-            return new QueryableTestScope(this);
+            var queryResult = await Context.Exec(query);
+            return queryResult.Rows;
+        }
+
+        public async Task CancelQuery(int millisecondDelay)
+        {
+            await Task.Delay(millisecondDelay);
+            Scope.GetInstance<CancellationTokenSource>().Cancel();
+        }
+
+        public async Task ResultTest<TComponent, TResult>(Action<TResult> test)
+        where TComponent : ResultProvider<TResult>
+        where TResult : class
+        {
+            // Resolve the component using the interface to ensure correct scope
+            var c = Scope.GetInstance<ResultProvider<TResult>>();
+            Assert.IsType<TComponent>(c);
+
+            var result = await c.ResultAsync;
+
+            test(result);
+        }
+
+        public async Task MetricsTest<T>(Action<IEnumerable<ExploreMetric>> test)
+        where T : PublisherComponent
+        {
+            var publisher = Scope.ResolvePublisherComponent<T>();
+
+            var metrics = new List<ExploreMetric>();
+            await foreach (var m in publisher.YieldMetrics())
+            {
+                metrics.Add(m);
+            }
+
+            test(metrics);
+        }
+
+        public void ConfigurePublisher<T>(Action<T> doSomething)
+        where T : PublisherComponent
+        {
+            var publisher = Scope.ResolvePublisherComponent<T>();
+            doSomething(publisher);
         }
 
         public void Dispose()
