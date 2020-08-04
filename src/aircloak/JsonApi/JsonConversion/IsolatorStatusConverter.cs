194feedcfd8d79b40f7643cca90118991c32deea
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-08-04 10:43:54 +0300

    disabled unused class warning for IsolatorStatusConverter

--- src/aircloak/JsonApi/JsonConversion/IsolatorStatusConverter.cs
+++ src/aircloak/JsonApi/JsonConversion/IsolatorStatusConverter.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CA1812 // IsolatorStatusConverter is an internal class that is apparently never instantiated. If so, remove the code from the assembly
 namespace Aircloak.JsonApi.JsonConversion
 {
     using System;
@@ -27,3 +28,4 @@ namespace Aircloak.JsonApi.JsonConversion
         }
     }
 }
+#pragma warning restore CA1812 // IsolatorStatusConverter is an internal class that is apparently never instantiated. If so, remove the code from the assembly
