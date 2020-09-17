b2140580f1f07b028fe99b86b435ddc74d67706a
dlennon <3168260+dandanlen@users.noreply.github.com>
2020-09-17 14:39:56 +0000

    Add explanations for `IsNull` and `IsSuppressed`.

--- src/explorer/Common/IndexedGroupingSetsResultMulti.cs
+++ src/explorer/Common/IndexedGroupingSetsResultMulti.cs
@@ -32,8 +32,30 @@ namespace Explorer.Common
         public IEnumerable<int> GroupingIndices =>
             GroupingIdConverter.GetConverter(AllGroupingLabels.Length).IndicesFromGroupingId(GroupingId);
 
+        /// <summary>
+        /// Gets a value indicating whether the row should be considered `null`. The row is considered null if *all*
+        /// columns are null.
+        /// <para>
+        /// Rationale:
+        /// Null is like nothing.
+        /// If all columns have nothing, then the row has nothing.
+        /// But if any column has something (not null) then the row has something.
+        /// </para>
+        /// </summary>
+        /// <returns><c>true</c> if all columns are null, otherwise <c>false</c>.</returns>
         public bool IsNull => Values.All(v => v.IsNull);
 
+        /// <summary>
+        /// Gets a value indicating whether the row should be considered suppressed. The row is considered suppressed
+        /// if *any* column is suppressed.
+        /// <para>
+        /// Rationale:
+        /// Suppressed values are uncertain.
+        /// If any column is uncertain than that makes the row uncertain.
+        /// Only if all columns are certain (not suppressed) can the row be considered certain.
+        /// </para>
+        /// </summary>
+        /// <returns><c>true</c> if any column is suppressed, otherwise <c>false</c>.</returns>
         public bool IsSuppressed => Values.Any(v => v.IsSuppressed);
 
         private ImmutableArray<TIndex> AllGroupingLabels { get; }
