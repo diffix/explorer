1fa70dbcf1d23ace6965933ee2435bfcc2bdc7b2
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-09-18 08:48:03 +0300

    use a typed metric for average estimate

---
+++ src/explorer/Common/MetricDefinitions.cs
@@ -0,0 +1,7 @@
+namespace Explorer.Common
+{
+    public static class MetricDefinitions
+    {
+        public static readonly MetricDefinition<decimal> AverageEstimate = new MetricDefinition<decimal>("average_estimate");
+    }
+}
