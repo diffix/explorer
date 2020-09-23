ba7ca47f5d7eea4084099c62fd63c16532030b23
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-09-23 09:50:38 +0300

    use typed metrics for min, max, count

--- src/explorer/Common/MetricDefinitions.cs
+++ src/explorer/Common/MetricDefinitions.cs
@@ -4,7 +4,20 @@ namespace Explorer.Common
 
     public static class MetricDefinitions
     {
+        public static readonly MetricDefinition<long> Count = new MetricDefinition<long>("count");
         public static readonly MetricDefinition<decimal> AverageEstimate = new MetricDefinition<decimal>("average_estimate");
         public static readonly MetricDefinition<IEnumerable<object>> SampleValues = new MetricDefinition<IEnumerable<object>>("sample_values");
+
+        public static MetricDefinition<T> Min<T>()
+        where T : unmanaged
+        {
+            return new MetricDefinition<T>("min");
+        }
+
+        public static MetricDefinition<T> Max<T>()
+        where T : unmanaged
+        {
+            return new MetricDefinition<T>("max");
+        }
     }
 }
