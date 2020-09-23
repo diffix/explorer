dc9746b9f54e7bb7396526f51016cb25e640cdb8
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-09-23 11:15:50 +0300

    change is_categorical to a typed metric removed the "distinct" prefix for is_categorical metric

--- src/explorer/Common/MetricDefinitions.cs
+++ src/explorer/Common/MetricDefinitions.cs
@@ -6,6 +6,7 @@ namespace Explorer.Common
     {
         public static readonly MetricDefinition<long> Count = new MetricDefinition<long>("count");
         public static readonly MetricDefinition<decimal> AverageEstimate = new MetricDefinition<decimal>("average_estimate");
+        public static readonly MetricDefinition<bool> IsCategorical = new MetricDefinition<bool>("is_categorical");
         public static readonly MetricDefinition<IEnumerable<object>> SampleValues = new MetricDefinition<IEnumerable<object>>("sample_values");
 
         public static MetricDefinition<T> Min<T>()
