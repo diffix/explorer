ca6b3d8a9424bf1ca4538700f1c5d81f69190031
Andrei Bozantan <andrei.bozantan@gmail.com>
2020-10-02 21:33:58 +0300

    clean-up web client code

--- src/clients/ExplorerWebClient/client.js
+++ src/clients/ExplorerWebClient/client.js
@@ -1,14 +1,70 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function explorerParams(dataSource, table, columns, apiURL, apiKey) {
+    return {
+        "DataSource": dataSource,
+        "Table": table,
+        "Columns": columns.constructor.name === "String" ? columns.split(",").map(s => s.trim()) : columns,
+        "ApiUrl": apiURL || AIRCLOAK_API_URL,
+        "ApiKey": apiKey || AIRCLOAK_API_KEY
+    };
 }
 
+async function explorerCancel(baseURL, state) {
+    if (state.exploration_id) {
+        console.log("cancelling exploration:", state.exploration_id);
+        const exploration_id = state.exploration_id;
+        state.exploration_id = null;
+        await _explorerGet(`${baseURL}/cancel/${exploration_id}`);
+        console.log("cancelled exploration:", exploration_id);
+    }
+}
 
-async function sfetch(method, endpoint, data, fn_show_error) {
+async function explorerRun(params, baseURL, state, fnShowResult) {
+    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    params = params.constructor.name === "Array" ? explorerParams(params[0], params[1], params[2], params[3], params[4]) : params;
+    baseURL = baseURL || EXPLORER_BASE_URL;
+    state = state || {};
+    let response = await _explorerPost(`${baseURL}/explore`, params);
+    let delay = 32;
+    let result_old = null;
+    state.exploration_id = response.id;
+    while (true) {
+        let result_text = JSON.stringify(response, undefined, 2);
+        if (result_text !== result_old) {
+            console.log(response);
+            result_old = result_text;
+            fnShowResult && fnShowResult(result_text);
+        }
+
+        await sleep(delay);
+        delay = Math.min(2 * delay, 1000);
+
+        if (response.status === "Complete" || response.Status === "Canceled" || response.Status === "Error") {
+            state.exploration_id = null;
+            return response;
+        }
+        if (state.exploration_id === null) {
+            console.log("stopping exploration:", response.id, "(exploration was cancelled)");
+            return null;
+        }
+
+        response = await _explorerGet(`${baseURL}/result/${state.exploration_id}`);
+    }
+}
+
+async function _explorerGet(endpoint) {
+    return await _explorerFetch('GET', endpoint, null);
+}
+
+async function _explorerPost(endpoint, data) {
+    return await _explorerFetch('POST', endpoint, data);
+}
+
+async function _explorerFetch(method, endpoint, data) {
     let options = {
         method: method,
         mode: 'cors',
         cache: 'no-cache',
-    };
+    }
 
     if (data) {
         options.headers = { 'Content-Type': 'application/json' };
@@ -16,93 +72,17 @@ async function sfetch(method, endpoint, data, fn_show_error) {
     }
 
     let result = null;
-
-    await fetch(`${EXPLORER_BASE_URL}/${endpoint}`, options)
+    await fetch(endpoint, options)
         .then(async response => {
             if (!response.ok) {
                 let details = await response.text();
                 console.error(response);
                 console.error(details);
-                show_error(response.statusText, details);
                 throw `${response.statusText}\n${details}`;
             }
             return response;
         })
         .then(async response => result = await response.json())
-        .catch(fn_show_error);
 
     return result;
-}
-
-async function fetch_get(endpoint, fn_show_error) {
-    return await sfetch('GET', endpoint, null, fn_show_error);
-}
-
-async function fetch_post(endpoint, data, fn_show_error) {
-    return await sfetch('POST', endpoint, data, fn_show_error);
-}
-
-function show_error(error, details) {
-    let div_result = document.getElementById("result");
-    div_result.innerHTML = `<div class='w3-text-dark-red'>${error}<br />${details || ""}</div>`;
-}
-
-let query_id = null;
-
-async function explore() {
-    try {
-        explore_impl();
-    }
-    finally {
-        query_id = null;
-    }
-}
-
-async function explore_impl() {
-    let div_result = document.getElementById("result");
-    let data_source = document.getElementById("txt_data_source").value;
-    let table_name = document.getElementById("txt_table").value;
-    let column_name = document.getElementById("txt_column").value;
-
-    let data = {
-        "DataSource": data_source,
-        "Table": table_name,
-        "Columns": column_name.split(",").map(s => s.trim()),
-        "ApiKey": AIRCLOAK_API_KEY,
-        "ApiUrl": AIRCLOAK_API_URL
-    };
-
-    let response = await fetch_post('explore', data, show_error);
-    let delay = 32;
-    let result_old = null;
-    query_id = response.id;
-    while (true) {
-        let result_text = JSON.stringify(response, undefined, 2);
-        if (result_text != result_old) {
-            console.log(response);
-            result_old = result_text;
-            div_result.innerText = result_text;
-        }
-
-        if (response.status == "Complete" || response.Status == "Canceled" || response.Status == "Error") {
-            return;
-        }
-        if (query_id == null) {
-            return;
-        }
-
-        response = await fetch_get(`result/${query_id}`, show_error);
-
-        await sleep(delay);
-        delay = Math.min(2 * delay, 1000);
-    }
-}
-
-function cancel() {
-    if (query_id) {
-        console.log("cancel:", query_id);
-        fetch_get(`cancel/${query_id}`, show_error);
-        show_error('');
-        query_id = null;
-    }
 }
